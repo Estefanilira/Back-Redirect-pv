4942a2ef2ae180a6d9c0f0a7b84b2b8eed389da4
Estefani Lira <estefaniwanderley.lira@gmail.com>
2024-05-28 00:52:55 -0300

    up

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("visibilitychange", handleVisibilityChange);
+window.addEventListener("beforeunload", handleBeforeUnload);
+window.addEventListener("popstate", handlePopstate);
+
+function redirectToBackRedirect() {
+    window.location.href = "http://127.0.0.1:5500/Backredrect.html";
+}
+
+function handleVisibilityChange() {
+    if (document.visibilityState === "hidden") {
+        redirectToBackRedirect();
+    }
+}
+
+function handleBeforeUnload(event) {
+    // console.log("event", event);
+    event.preventDefault();
+    redirectToBackRedirect();
+}
+
+function handlePopstate() {
+    redirectToBackRedirect();
+}
