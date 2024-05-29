3c0c5b965c3b3be64320502639ed65bff0367361
Estefani Lira <estefaniwanderley.lira@gmail.com>
2024-05-29 17:58:38 -0300

    script time

--- script.js
+++ script.js
@@ -23,36 +23,3 @@ function handlePopstate() {
     redirectToBackRedirect();
 }
 
-// TIME
-
-let minutes = 15;
-let seconds = 0;
-
-function startTimer() {
-    const minutesElement = document.getElementById('minutes');
-    const secondsElement = document.getElementById('seconds');
-
-    const countdown = setInterval(() => {
-        if (seconds === 0) {
-            if (minutes === 0) {
-                clearInterval(countdown);
-                return;
-            }
-            minutes--;
-            seconds = 59;
-        } else {
-            seconds--;
-        }
-
-        minutesElement.textContent = minutes < 10 ? '0' + minutes : minutes;
-        secondsElement.textContent = seconds < 10 ? '0' + seconds : seconds;
-    }, 1000);
-}
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    startTimer();
-});
-
-document.getElementById('use-coupon-button').addEventListener('click', () => {
-    window.location.href = "backredirect.html";
-});
